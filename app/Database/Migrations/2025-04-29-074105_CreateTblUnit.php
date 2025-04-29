8e202cbf352084e96fc231f5c2edfc1faebd4ea5
loudik <loudikmarkai@gmail.com>
2025-04-29 16:42:36 +0900

    Update View

---
+++ app/Database/Migrations/2025-04-29-074105_CreateTblUnit.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateTblUnit extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'auto_increment' => true,
+                'unsigned'       => true,
+            ],
+            'unitname' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 150,
+                'null'       => true,
+            ],
+            'unitcode' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 20,
+                'null'       => true,
+            ],
+            'remark' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'iby' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 30,
+                'null'       => true,
+            ],
+            'idt' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'uby' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 30,
+                'null'       => true,
+            ],
+            'udt' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'isdeleted' => [
+                'type'       => 'SMALLINT',
+                'constraint' => 3,
+                'default'    => 0,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true); // Primary key
+        $this->forge->createTable('tbl_unit');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tbl_unit');
+    }
+}
